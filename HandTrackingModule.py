ed9478c280a84e52b59045e3ac7eac8fed409914
nimabgr <nimabagherzadehw@gmail.com>
2025-10-12 00:34:29 +0330

    adding q option to close camera

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -20,7 +20,7 @@ class handDetector():
     def findHands(self, img , draw=True ):
         imgRGB = cv2.cvtColor(img , cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        #print(results.multi_hand_landmarks)
+        
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
@@ -37,7 +37,7 @@ class handDetector():
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                #print(id , cx , cy)
+                
                 lmList.append([id , cx , cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
@@ -63,7 +63,8 @@ def main():
 
         cv2.putText(img , str(int(fps)), (10,70) , cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
         cv2.imshow("Image" , img)
-        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     
 if __name__ == "__main__":
